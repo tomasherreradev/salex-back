8a2657fde55cff263a335e074da1a30917f60e81
tomasherreradev <herreratomas270@gmail.com>
2024-09-19 11:59:46 -0300

    first commit

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import db from '../config/db';
+import db from '../../config/db';
 import { User } from '../models/userModel';
 import { ResultSetHeader } from 'mysql2';
 
